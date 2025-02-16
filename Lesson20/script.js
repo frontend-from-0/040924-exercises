8f714a6f1294163486715a658631018fe47d6499
Busra-Demirkesen <busrademirkesen2@gmail.com>
2025-02-16 22:04:55 +0100

    Refactor Todo app: streamline event listeners and enhance task completion functionality

--- Lesson20/script.js
+++ Lesson20/script.js
@@ -4,8 +4,6 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 
-
-
 const today = new Date();
 const formattedDate = today.toISOString().split("T")[0];
 document.getElementById("date-display").innerText = formattedDate;
@@ -17,7 +15,7 @@ document.getElementById("todoForm").addEventListener("submit", (event) => {
   event.preventDefault();
 
   const todoInput = document.getElementById("todoInput");
-  const todoInputValue = todoInput.value.trim(); 
+  const todoInputValue = todoInput.value.trim();
 
   if (!todoInputValue) {
     alert("Please enter a valid task");
@@ -40,20 +38,22 @@ function addTodoToUI(todoText) {
   spanElement.classList.add("todo-text");
   spanElement.textContent = todoText;
 
-
   const checkTodoBtn = document.createElement("button");
   checkTodoBtn.textContent = "✓";
   checkTodoBtn.classList.add("btn-success");
-
 
   let removeTodoBtn = document.createElement("button");
   removeTodoBtn.textContent = "X";
   removeTodoBtn.classList.add("btn-danger");
 
-
   removeTodoBtn.addEventListener("click", () => {
     listItemElement.remove();
     removeTodoFromStorage(todoText);
+  });
+
+ 
+  checkTodoBtn.addEventListener("click", () => {
+    listItemElement.classList.toggle("completed");
   });
 
   listItemElement.appendChild(spanElement);
@@ -63,29 +63,16 @@ function addTodoToUI(todoText) {
   document.getElementById("list").appendChild(listItemElement);
 }
 
-checkTodoBtn.addEventListener("click", () => {
-  listItemElement.classList.toggle("checked");
-});
-
-
 function removeTodoFromStorage(todoText) {
   let prevTodos = JSON.parse(localStorage.getItem("tasks")) || [];
   prevTodos = prevTodos.filter((todo) => todo !== todoText);
   localStorage.setItem("tasks", JSON.stringify(prevTodos));
 }
 
-
-
-
-
 function loadTodos() {
   let prevTodos = JSON.parse(localStorage.getItem("tasks")) || [];
   prevTodos.forEach((todo) => addTodoToUI(todo));
 }
-
-
-
-
 
 document.getElementById("clearListBtn").addEventListener("click", function () {
   localStorage.removeItem("tasks");
