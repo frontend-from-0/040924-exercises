224a236b8c91c4edf416bd3df0c657f2ecb8d6fb
duyguozpercin <duygucetind@gmail.com>
2025-02-19 17:16:02 +0300

    added codes in js

--- Lesson20/todo-app/duygu/script.js
+++ Lesson20/todo-app/duygu/script.js
@@ -10,8 +10,36 @@ function handleTodoFormSubmit(event) {
   const todoInputValue = todoInput.value;
   console.log(todoInputValue);
 
+  /* const prevTodos = localStorage.getItem("tasks");
+  localStorage.setItem('todos', [prevTodos, todoInputValue]); */
   const prevTodos = JSON.parse(localStorage.getItem("tasks")) || [];
   prevTodos.push(todoInputValue);
   localStorage.setItem("tasks", JSON.stringify(prevTodos));
+
+  const listItemElement = document.createElement("li");
+  console.log(listItemElement);
+  listItemElement.classList.add("todo-list-item");
+  console.log(listItemElement);
+
+  const spanElement = document.createElement("span");
+  spanElement.classList.add("todo-text");
+  console.log(spanElement);
+  spanElement.textContent = todoInputValue;
+
+  const removeTodoBtn = document.createElement("button");
+  removeTodoBtn.textContent = "X";
+  removeTodoBtn.classList.add("btn-danger");
+  removeTodoBtn.addEventListener('click', () => {
+    listItemElement.remove();
+  });
+
+  listItemElement.appendChild(spanElement);
+  listItemElement.appendChild(removeTodoBtn);
+
+  const ulElement = document.getElementById("list");
+  ulElement.appendChild(listItemElement);
+  todoInput.value = "";
+
+
 
 }
