e06621968287fe10189222ecdcf7520104a3e847
duyguozpercin <duygucetind@gmail.com>
2025-02-19 14:40:46 +0300

    updated js files

--- Lesson20/todo-app/duygu/script.js
+++ Lesson20/todo-app/duygu/script.js
@@ -1,1 +1,17 @@
-document.getElementById("todoForm").addEventListener("submit")
+document.getElementById("todoForm")
+  .addEventListener("submit", (event) => handleTodoFormSubmit(event));
+
+function handleTodoFormSubmit(event) {
+  event.preventDefault();
+  console.log(event);
+  const todoInput = document.getElementById("todoInput");
+  console.log(todoInput);
+
+  const todoInputValue = todoInput.value;
+  console.log(todoInputValue);
+
+  const prevTodos = JSON.parse(localStorage.getItem("tasks")) || [];
+  prevTodos.push(todoInputValue);
+  localStorage.setItem("tasks", JSON.stringify(prevTodos));
+
+}
