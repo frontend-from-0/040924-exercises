230ac6cd1cdcd13819824edc38c269f82a72d7b4
tgbypc <tugbaypc.11@gmail.com>
2025-02-25 12:37:59 +0100

    lesson21/Tugba

--- Lesson21/tugba/script.js
+++ Lesson21/tugba/script.js
@@ -1,20 +1,17 @@
-document.addEventListener("DOMContentLoaded", function () {
-
-  const accordionItems = document.querySelectorAll(".item");
+document.addEventListener('DOMContentLoaded', function () {
+  const accordionItems = document.querySelectorAll('.item');
 
   accordionItems.forEach((item) => {
-    const title = item.querySelector(".title");
+    const title = item.querySelector('.title');
 
-    title.addEventListener("click", function () {
-  
+    title.addEventListener('click', function () {
       accordionItems.forEach((otherItem) => {
         if (otherItem !== item) {
-          otherItem.classList.remove("active");
+          otherItem.classList.remove('active');
         }
       });
 
-     
-      item.classList.toggle("active");
+      item.classList.toggle('active');
     });
   });
 });
