6b2c6da912b03fe090b77cb1604c30e8b110fb46
Anna S. A <anna.atalay@minna.tech>
2025-02-26 20:52:18 +0100

    Fix formatting

--- Lesson21/Busra/script.js
+++ Lesson21/Busra/script.js
@@ -1,21 +1,16 @@
-const questionsAnswers = document.querySelectorAll(".question-answer");
+const questionsAnswers = document.querySelectorAll('.question-answer');
 
-const titleQuestion = document.querySelectorAll(".title_question");
+const titleQuestion = document.querySelectorAll('.title_question');
 
-
-
-questionsAnswers.forEach(function(question) {
- 
-    const button = question.querySelector(".question_button");
+questionsAnswers.forEach(function (question) {
+  const button = question.querySelector('.question_button');
   button.addEventListener('click', function () {
-  
-    questionsAnswers.forEach(function(item) {
+    questionsAnswers.forEach(function (item) {
       if (item !== question) {
-        item.classList.remove("show-answer");
+        item.classList.remove('show-answer');
       }
     });
 
-   
-    question.classList.toggle("show-answer");
+    question.classList.toggle('show-answer');
   });
 });
