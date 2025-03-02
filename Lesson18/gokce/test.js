3b87793b79bd94a09934416c9c6e2968bc782ecc
Gokcekrt <a.gokcekurt@gmail.com>
2025-03-02 12:05:00 +0300

    Lesson18-Gokce-extraAssşgnnment-fourth_commit

--- Lesson18/gokce/test.js
+++ Lesson18/gokce/test.js
@@ -1,20 +1,15 @@
 const { Restaurant, MenuItem, Order } = require('./extraAssignment.js');
 const myRestaurant = new Restaurant('ASPAVA', 'ANKARA');
-
 myRestaurant.addMenuItem('cake', 17, 'dessert');
 myRestaurant.addMenuItem('pizza', 25, 'main course');
 myRestaurant.addMenuItem('salad', 10, 'appetizer');
-
 myRestaurant.viewMenu();
-
 const myOrder = new Order();
-
 myOrder.addItem('cake', 2, 17, 'dessert');
-
 myRestaurant.placeOrder(myOrder);
 myRestaurant.listAllOrders();
 console.log(
-  'Total of all orders: $' + myRestaurant.calculateTotalOfAllOrders()
+  'Total of all order: $' + myRestaurant.calculateTotalOfAllOrders()
 );
 
 console.log(
