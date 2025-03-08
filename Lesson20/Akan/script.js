4129c403814347102d8bd11d6204802bd4e0d2ed
Akan-86 <akan.degirmenci@gmail.com>
2025-03-08 14:12:43 +0300

    Fix formating

--- Lesson20/Akan/script.js
+++ Lesson20/Akan/script.js
@@ -1,17 +1,20 @@
 document.addEventListener('DOMContentLoaded', () => {
-
   const todos = JSON.parse(localStorage.getItem('todos')) || [];
-  todos.forEach(todo => addTodoToDOM(todo));
+  todos.forEach((todo) => addTodoToDOM(todo));
 
-  document.getElementById("todoForm").addEventListener("submit", (event) => handleTodoFormSubmit(event));
+  document
+    .getElementById('todoForm')
+    .addEventListener('submit', (event) => handleTodoFormSubmit(event));
 
-  document.getElementById("clearListBtn").addEventListener("click", clearTodoList);
+  document
+    .getElementById('clearListBtn')
+    .addEventListener('click', clearTodoList);
 });
 
 function handleTodoFormSubmit(event) {
   event.preventDefault();
 
-  const todoInput = document.getElementById("todoInput");
+  const todoInput = document.getElementById('todoInput');
   const todoInputValue = todoInput.value.trim();
 
   if (todoInputValue) {
@@ -23,36 +26,38 @@ function handleTodoFormSubmit(event) {
 
     addTodoToDOM(newTodo);
 
-    todoInput.value = "";
+    todoInput.value = '';
   }
 }
 
 function addTodoToDOM(todo) {
-  const listItemElement = document.createElement("li");
-  listItemElement.classList.add("todo-list-item");
+  const listItemElement = document.createElement('li');
+  listItemElement.classList.add('todo-list-item');
 
-  const spanElement = document.createElement("span");
-  spanElement.classList.add("todo-text");
+  const spanElement = document.createElement('span');
+  spanElement.classList.add('todo-text');
   spanElement.textContent = todo.text;
   if (todo.completed) {
-    spanElement.classList.add("completed");
+    spanElement.classList.add('completed');
   }
 
-  spanElement.addEventListener("click", () => {
+  spanElement.addEventListener('click', () => {
     todo.completed = !todo.completed;
     localStorage.setItem('todos', JSON.stringify(todos));
-    spanElement.classList.toggle("completed");
+    spanElement.classList.toggle('completed');
   });
 
-  const removeTodoBtn = document.createElement("button");
-  removeTodoBtn.textContent = "X";
-  removeTodoBtn.classList.add("btn-danger");
-  removeTodoBtn.addEventListener("click", () => removeTodo(listItemElement, todo));
+  const removeTodoBtn = document.createElement('button');
+  removeTodoBtn.textContent = 'X';
+  removeTodoBtn.classList.add('btn-danger');
+  removeTodoBtn.addEventListener('click', () =>
+    removeTodo(listItemElement, todo)
+  );
 
   listItemElement.appendChild(spanElement);
   listItemElement.appendChild(removeTodoBtn);
 
-  const ulElement = document.getElementById("list");
+  const ulElement = document.getElementById('list');
   ulElement.appendChild(listItemElement);
 }
 
@@ -60,13 +65,13 @@ function removeTodo(listItemElement, todo) {
   listItemElement.remove();
 
   const todos = JSON.parse(localStorage.getItem('todos')) || [];
-  const updatedTodos = todos.filter(t => t.text !== todo.text);
+  const updatedTodos = todos.filter((t) => t.text !== todo.text);
   localStorage.setItem('todos', JSON.stringify(updatedTodos));
 }
 
 function clearTodoList() {
-  const ulElement = document.getElementById("list");
-  ulElement.innerHTML = "";
+  const ulElement = document.getElementById('list');
+  ulElement.innerHTML = '';
 
-  localStorage.removeItem("todos");
+  localStorage.removeItem('todos');
 }
