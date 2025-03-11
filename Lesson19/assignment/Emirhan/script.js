ed5d59208256fc970c851f24d6c63562e5e9faf1
emirhan.bayatli <emirhan.bayatli99@icloud.com>
2025-03-11 13:53:58 +0300

    Added alt element for image and problems solved

--- Lesson19/assignment/Emirhan/script.js
+++ Lesson19/assignment/Emirhan/script.js
@@ -13,26 +13,29 @@ document.getElementById('update-image').addEventListener('click', function () {
     'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'.alt =
       'a women profile picture';
 });
-let counter = 0;
+
 // 3. Change Theme
-document.getElementById('change-theme').addEventListener('click', function () {
+ document.getElementById('change-theme').addEventListener('click', function () {
   // TODO: Toggle the background color of the ".profile-card" between white (#fff) and lightblue (#add8e6)
-  counter++;
-  if (counter % 2) {
-    document.querySelector('.profile-card').style.backgroundColor = '#add8e6';
-  } else {
-    document.querySelector('.profile-card').style.backgroundColor = '#fff';
-  }
+    const theme =  document.querySelector('.profile-card');
+    if (!theme.classList.contains('theme-blue')){
+      theme.classList.add('theme-blue');
+    }else{
+      theme.classList.remove('theme-blue');
+    }
+    
 });
 // 4. Toggle Description
 document.getElementById('toggle-desc').addEventListener('click', function () {
   // TODO: Toggle the display property of the element with ID "profile-desc" between "none" and its default display.
   const profileDesc = document.getElementById('profile-desc');
-  if (profileDesc.style.display === 'none') {
-    document.getElementById('profile-desc').style.display = 'block';
-  } else {
-    document.getElementById('profile-desc').style.display = 'none';
+
+  if (!profileDesc.classList.contains('toggle-desc-active')){
+    profileDesc.classList.add('toggle-desc-active');
+  }else{
+    profileDesc.classList.remove('toggle-desc-active');
   }
+  
 });
 
 // 5. Add Social Media Link
