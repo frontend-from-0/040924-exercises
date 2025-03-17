05a19b0fc5710be1256b97fc5979b2154a72d4cf
Akan-86 <akan.degirmenci@gmail.com>
2025-03-17 21:30:56 +0300

    Lesson22-Akan

---
+++ Lesson22/Akan/app.js
@@ -0,0 +1,64 @@
+let currentImageIndex = 0;
+const images = document.querySelectorAll('.carousel img');
+const indicators = document.querySelectorAll('.indicator');
+const imageArrayLength = images.length;
+let autoChangeInterval;
+
+
+function updateActiveImage() {
+ 
+  images.forEach((image, index) => {
+    image.classList.remove('active');
+    indicators[index].classList.remove('active');
+  });
+
+  
+  images[currentImageIndex].classList.add('active');
+  indicators[currentImageIndex].classList.add('active');
+}
+
+
+function startAutoChange() {
+  autoChangeInterval = setInterval(() => {
+    currentImageIndex = (currentImageIndex + 1) % imageArrayLength; 
+    updateActiveImage();
+  }, 3000); 
+}
+
+
+function stopAutoChange() {
+  clearInterval(autoChangeInterval);
+}
+
+document.getElementById('next-btn').addEventListener('click', function () {
+  currentImageIndex = (currentImageIndex + 1) % imageArrayLength; 
+  updateActiveImage();
+});
+
+
+document.getElementById('prev-btn').addEventListener('click', function () {
+  currentImageIndex = (currentImageIndex - 1 + imageArrayLength) % imageArrayLength; 
+  updateActiveImage();
+});
+
+
+indicators.forEach((indicator, index) => {
+  indicator.addEventListener('click', function () {
+    currentImageIndex = index; 
+    updateActiveImage();
+  });
+});
+
+
+document.getElementById('start-stop-btn').addEventListener('click', function () {
+  if (autoChangeInterval) {
+    stopAutoChange(); 
+    this.textContent = 'Start'; 
+  } else {
+    startAutoChange(); 
+    this.textContent = 'Stop'; 
+  }
+});
+
+
+startAutoChange();
