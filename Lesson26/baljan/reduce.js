1df1f28bc20744a9cdb3c8eaf1f89daa38092016
Baljan Kanatbek <baljann19@gmail.com>
2025-04-01 21:58:59 +0300

    Remove comments from reduce.js

--- Lesson26/baljan/reduce.js
+++ Lesson26/baljan/reduce.js
@@ -1,14 +1,3 @@
-/* array.reduce(callback(accumulator, currentValue, currentIndex, array), initialValue);
-
-- callback: A function that is executed on each element of the array. It takes four arguments:
-- accumulator: The accumulated value that is returned after each iteration. It starts with the value of initialValue on the first iteration and is the result of the previous iteration in subsequent iterations.
-- currentValue: The current element being processed in the array.
-- currentIndex: The index of the current element being processed.
-- array: The array on which reduce() was called.
-
-- initialValue: An optional initial value for the accumulator. If not provided, the first element of the array is used as the initial value and the iteration starts from the second element.
-*/
-
 // Exercise 1:
 // Calculate the sum of all numbers in the given array.
 const numbersEx1 = [1, 2, 3, 4, 5];
