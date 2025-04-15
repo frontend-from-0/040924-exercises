6ef2b0ca1dde0f0d278ae0fdae5a24533a3fb942
tgbypc <tugbaypc.11@gmail.com>
2025-04-15 11:52:06 +0200

    lesson27-tugba

--- Lesson27/tugba/script.js
+++ Lesson27/tugba/script.js
@@ -8,11 +8,23 @@ const personE17 = {
   },
 };
 
+const {
+  name,
+  age,
+  address: { city },
+} = personE17;
+
+console.log(name);
+console.log(age);
+console.log(city);
+
 // 7. Default Parameters
 // Add Default Parameters to an Existing Arrow Function, Default tax rate 0.1, default discount is 0.
-const calculateTotal = (price, taxRate, discount) => {
+const calculateTotal = (price, taxRate = 0.1, discount = 0) => {
   return price + price * taxRate - discount;
 };
+
+console.log(calculateTotal(100)); // 110
 
 // 9. Handle Missing Properties
 // Update the code to use optional chaining to safely access userCountry (userEx9.profile.address.country) and provide a default value of 'Unknown' if the property is missing.
@@ -23,6 +35,10 @@ const userEx9 = {
   },
 };
 
+const userCountry = userEx9.profile?.address?.country ?? 'Unknown';
+
+console.log(userCountry);
+
 // 10. Optional Chaining with Function Calls
 // Update the code to safely call the getName function using optional chaining, considering that profile or getName might be missing.
 
@@ -32,17 +48,20 @@ const userEx10 = {
   },
 };
 
+const name10 = userEx10.profile?.getName?.();
+console.log(name10);
+
 // 11. Rewrite the code using the nullish coalescing operator to assign a default value to storedData only if userInput is null or undefined.
 let userInput;
 let storedData = userInput ? userInput : 'Default Value';
 
-console.log(storedData); // Default Value
+console.log(storedData);
 
 // 12. Rewrite the code using the nullish coalescing operator to display number of users even if it is 0.
 let userCount = 0;
 let displayCount = userCount || 'No users';
 
-console.log(displayCount); // No users
+console.log(displayCount);
 
 // 13. Rewrite the code using the nullish coalescing operator to assign a default value of 3000 to timeout if config.timeout is null or undefined.
 
@@ -55,4 +74,4 @@ const timeout =
     ? config.timeout
     : 3000;
 
-console.log(timeout); // 3000
+console.log(timeout);
