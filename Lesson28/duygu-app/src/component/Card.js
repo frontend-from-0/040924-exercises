b3bddb51c817f9f1f0613d4c90625e3104e8cf06
duyguozpercin <duygucetind@gmail.com>
2025-04-23 18:57:54 +0300

    All js and css files created

---
+++ Lesson28/duygu-app/src/component/Card.js
@@ -0,0 +1,20 @@
+import './Card.css';
+
+export function Card({ cake }) {
+  return (
+    <div className="Card" style={cake.theme}>
+      <h1>{cake.name}</h1>
+      <img
+        className="avatar"
+        src={cake.image}
+        alt={cake.name}
+      />
+      <ul>
+        {cake.tasks.map((task, index) => (
+          <li key={index}>{task}</li>
+        ))}
+      </ul>
+      <button className="card-button">View Recipe</button>
+    </div>
+  );
+}
