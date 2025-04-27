3a574e8352334f1234df525793b1a81f4e045af1
duyguozpercin <duygucetind@gmail.com>
2025-04-27 23:54:26 +0300

    changed

--- Lesson28/duygu-app/src/component/Navbar.js
+++ Lesson28/duygu-app/src/component/Navbar.js
@@ -14,6 +14,7 @@ export function Navbar() {
         {menuItems.map((item) => (
           <li key={item.name}>
             <a href={item.href}>{item.name}</a>
+            
           </li>
         ))}
       </ul>
