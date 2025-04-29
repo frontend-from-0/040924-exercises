003fa7eac956da4961a8d7ad01654a85386feadb
duyguozpercin <duygucetind@gmail.com>
2025-04-29 16:51:47 +0300

    Refactor App component and add QuoteCard component with styles

--- Lesson29/duygu/duygu/src/App.js
+++ Lesson29/duygu/duygu/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import {quotes as intialQuotes} from './quotes';
 import './App.css';
+import {QuoteCard} from './components/QuoteCard';
+import {useState} from 'react';
 
 function App() {
+  // const [state, setState] = useState(initialState)
+  const [quotes, setQuotes] = useState(intialQuotes);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+
+  function handleNextQuoteClick () {
+    const randomIndex = Math.floor(Math.random() * quotes.length);
+    // THIS IS NOT CORRECT: currentIndex = randomIndex;
+    setCurrentIndex(randomIndex);
+
+  }
+
   return (
+    // JSX
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <QuoteCard quote={quotes[currentIndex].quote} author={quotes[currentIndex].author} />
+
+      <button onClick={handleNextQuoteClick}>Next quote</button>
     </div>
   );
 }
