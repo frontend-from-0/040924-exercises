0614d0066f5b57d003404c197cdfe428beec2d5b
duyguozpercin <duygucetind@gmail.com>
2025-04-29 17:25:14 +0300

    Add cake images and refactor App component to display cake cards

--- Lesson28/duygu/src/App.js
+++ Lesson28/duygu/src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import { Navbar } from './components/Navbar';
+import { Card } from './components/Card';
 
-function App() {
+const cakes = [
+  {
+    name: 'Strawberry Cake',
+    image: 'strawberrycake.png',
+    theme: {
+      backgroundColor: '#fff0f6',
+      color: '#880e4f',
+    },
+    tasks: [
+      'Bake the sponge cake in the oven.',
+      'Spread cream between the layers and add fresh strawberries.',
+      'Cover the top with whipped cream and decorate with strawberries.',
+    ],
+  },
+  {
+    name: 'Tiramisu',
+    image: 'tiramisu.png',
+    theme: {
+      backgroundColor: '#f3e5ab',
+      color: '#4e342e',
+    },
+    tasks: [
+      'Soak the ladyfinger biscuits in coffee',
+      'Layer with mascarpone cream.',
+      'Dust with cocoa powder and chill in the fridge for a few hours.',
+    ],
+  },
+];
+
+export function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <main className="CardWrapper">
+        {cakes.map((cake) => (
+          <Card key={cake.name} cake={cake} />
+        ))}
+      </main>
     </div>
   );
 }
