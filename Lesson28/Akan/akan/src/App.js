5243cca0fae9dcf4a018238906ca4c81c9faedfb
Akan-86 <akan.degirmenci@gmail.com>
2025-05-01 18:56:04 +0300

    curly brackets are removed and fix formatting

--- Lesson28/Akan/akan/src/App.js
+++ Lesson28/Akan/akan/src/App.js
@@ -9,7 +9,6 @@ function App() {
     <div className="App">
       <Navbar />
 
-      {}
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
