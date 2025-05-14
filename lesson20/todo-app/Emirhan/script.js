2867952547a45e66d6ac7f178d7ae6999a3f9268
emirhan.bayatli <emirhan.bayatli99@icloud.com>
2025-05-14 12:39:40 +0300

    Refactor HTML structure and improve CSS styles; update error handling in JavaScript

--- lesson20/todo-app/Emirhan/script.js
+++ lesson20/todo-app/Emirhan/script.js
@@ -1,70 +1,56 @@
-function showToast(message) {
-  const toast = document.createElement('div');
-  toast.classList.add('error-message');
-  toast.textContent = message;
-  document.body.appendChild(toast);
+function errorMessage(message) {
+  const error = document.createElement('div');
+  error.classList.add('error-message');
+  error.textContent = message;
+  document.body.appendChild(error);
   setTimeout(() => {
-    toast.remove();
+    error.remove();
   }, 3000);
 }
 
 document
-  .getElementById("todoForm")
-  .addEventListener("submit", (event) => handleTodoFormSubmit(event));
+  .getElementById('todoForm')
+  .addEventListener('submit', (event) => handleTodoFormSubmit(event));
 
 function handleTodoFormSubmit(event) {
   event.preventDefault();
-  console.log(event);
-  const todoInput = document.getElementById("todoInput");
-  console.log(todoInput);
-
+  const todoInput = document.getElementById('todoInput');
   const todoInputValue = todoInput.value;
-  console.log(todoInputValue);
+  console.log(todoInputValue + ' VALUE OF INPUT');
 
   if (todoInputValue === '' || todoInputValue === ' ') {
-    showToast('This field cannot be left blank');
+    errorMessage('This field cannot be left blank');
     return;
   }
 
-  // If we get data from localStorage directly, we will get in a string format. 
+  // If we get data from localStorage directly, we will get in a string format.
   // To get it in a form of array, we need to parse it using JSON.parse() method:
   // let prevTodos = JSON.parse(localStorage.getItem("tasks")) || [];
   // We well talk more about JSON after a few lessons.
 
-
-  const prevTodos = JSON.parse(localStorage.getItem("Todos")) || [];
+  //localStorage
+  const prevTodos = JSON.parse(localStorage.getItem('Todos')) || [];
   prevTodos.push(todoInputValue);
-  localStorage.setItem("Todos", JSON.stringify(prevTodos));
+  localStorage.setItem('Todos', JSON.stringify(prevTodos));
 
-  const listItemElement = document.createElement("li");
-  console.log(listItemElement);
+  const listItemElement = document.createElement('li');
+  listItemElement.classList.add('todo-list-item');
 
-  listItemElement.classList.add("todo-list-item");
-  console.log(listItemElement);
-
-  const spanElement = document.createElement("span");
-  spanElement.classList.add("todo-text");
-  console.log(spanElement);
+  const spanElement = document.createElement('span');
+  spanElement.classList.add('todo-text');
   spanElement.textContent = todoInputValue;
 
-  const removeTodoBtn = document.createElement("i");
-  //removeTodoBtn.textContent = "X";
-  removeTodoBtn.classList.add("btn-danger");
-  removeTodoBtn.classList.add("fa-solid");
-  removeTodoBtn.classList.add("fa-xmark");
+  const removeTodoBtn = document.createElement('i');
+  removeTodoBtn.classList.add('btn-remove', 'fa-solid', 'fa-xmark');
   removeTodoBtn.addEventListener('click', () => {
     listItemElement.remove();
   });
 
-
   listItemElement.appendChild(spanElement);
   listItemElement.appendChild(removeTodoBtn);
-
-  const ulElement = document.getElementById("list");
-
+  const ulElement = document.getElementById('list');
   ulElement.appendChild(listItemElement);
-
-  todoInput.value = "";
+  todoInput.value = '';
 
   /*
   create span element, add text, add class
@@ -72,17 +58,14 @@ function handleTodoFormSubmit(event) {
 
    */
 
-  spanElement.addEventListener("click", function () {
-    spanElement.classList.add("completed");
+  spanElement.addEventListener('click', function () {
+    spanElement.classList.add('completed');
   });
 }
 
-
-
-document.getElementById("clearListBtn").addEventListener("click", function () {
-  const liElements = document.querySelectorAll("li");
-  console.log(liElements);
-  liElements.forEach((li) => (li.innerText = ""));
+document.getElementById('clearListBtn').addEventListener('click', function () {
+  const liElements = document.querySelectorAll('li');
+  liElements.forEach((li) => li.remove());
   localStorage.clear();
 });
 
