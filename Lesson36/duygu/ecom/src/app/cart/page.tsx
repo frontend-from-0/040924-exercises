33d6230af13484f3d62a66a1db974fbcd5d24460
duyguozpercin <duygucetind@gmail.com>
2025-06-21 23:39:47 +0300

    Add initial setup for Ecom website with global styles, layout, and product pages
    - Created globals.css for styling with Tailwind CSS and dark mode support.
    - Implemented layout.tsx to define the root layout and include Google fonts.
    - Added a home page (page.tsx) with a title for the Ecom website.
    - Created a product details page ([productId]/page.tsx) with a title.
    - Configured TypeScript with tsconfig.json for strict type checking and module resolution.

---
+++ Lesson36/duygu/ecom/src/app/cart/page.tsx
@@ -0,0 +1,7 @@
+import Image from "next/image";
+
+export default function Cart() {
+  return (
+   <h1 className="text-center m-12">Cart</h1>
+  );
+}
---
+++ Lesson36/duygu/ecom/src/app/products/[productId]/page.tsx
@@ -0,0 +1,7 @@
+import Image from "next/image";
+
+export default function ProductDetails() {
+  return (
+   <h1 className="text-center m-12">Product Details</h1>
+  );
+}
